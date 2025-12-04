a886bfe59328615c4415a42b7214a2160c75c871
John Mitchell <johnnymitchell24@icloud.com>
2025-12-04 10:58:50 -0700

    Fixed exporter and main to match refactored Graph and Parser APIs

--- src/main/java/edu/asu/graph/Main.java
+++ src/main/java/edu/asu/graph/Main.java
@@ -2,32 +2,25 @@ package edu.asu.graph;
 
 public class Main {
 
-    // args:
-    // [0] input DOT file path
-    // [1] output text summary file path
-    // [2] output PNG file path
     public static void main(String[] args) {
-        if (args.length < 3) {
-            System.out.println("Usage: java Main <input.dot> <out.txt> <out.png>");
-            return;
-        }
-
-        String dotIn = args[0];
-        String summaryOut = args[1];
-        String pngOut = args[2];
-
         try {
-            Graph g = GraphParser.parseGraph(dotIn);
+            Graph g = GraphParser.parseDotFile("sample.dot");
 
-            System.out.println("Parsed graph:");
-            System.out.println(g.toString());
+            Node src = new Node("a");
+            Node dst = new Node("c");
 
-            g.outputGraph(summaryOut);
+            System.out.println("BFS Search:");
+            Path bfsPath = g.graphSearch(src, dst, Algorithm.BFS);
+            System.out.println(bfsPath);
 
-            GraphExporter.outputGraphics(g, pngOut, "png");
+            System.out.println("\nDFS Search:");
+            Path dfsPath = g.graphSearch(src, dst, Algorithm.DFS);
+            System.out.println(dfsPath);
 
-            System.out.println("Wrote summary to " + summaryOut);
-            System.out.println("Wrote PNG to " + pngOut);
+            System.out.println("\nRandom Walk Search:");
+            Path randomPath = g.graphSearch(src, dst, Algorithm.RANDOM);
+            System.out.println(randomPath);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
